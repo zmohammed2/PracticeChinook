6ab347ef37644cc5f4910ed429e1a5849400b1e6
zmohammed2 <zmohammed2@studentmail.nait.ca>
2016-09-27 16:33:50 -0600

    Set DAL And BLL

---
+++ ChinookSample/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel; //ODS
+using ChinookSystem.Data.Entities;
+using ChinookSystem.Data.POCOs;
+using ChinookSystem.DAL;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+   public class ArtistController
+    {
+        //report a dataset containing data from 
+        //multiple entities
+        //this will use Linq to Entity Access 
+        //POCO Classes will be use to define the data
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<ArtistAlbums> ArtistAlbums_Get()
+        {
+            //Set up Transaction Area 
+            using (var context = new ChinookContext())
+            {
+                //when you bring the query from linq pad to your program you must change the refrences to the datasource
+
+                //you may also need to change your navigation refrencing use in linqPad
+                //to the navigation properties you stated in the Entity class definitions
+                var results = from x in context.Albums
+                              where x.ReleaseYear == 2008
+                              orderby x.Artists.Name, x.Title
+                              select new ArtistAlbums
+                              {
+                                  //Name and Titles are POCO 
+                                  //Class property names
+                                  Name = x.Artists.Name,
+                                 Title = x.Title
+                              };
+                //the following requires the query data in memory
+                //.ToList()
+                //At this point the query will actually execute
+                return results.ToList();
+            }
+        }
+    }
+}
---
+++ ChinookSample/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using ChinookSystem.Data.Entities;
+#endregion
+
+namespace ChinookSystem.DAL
+{
+    //iinternal for security reasons 
+    //access restricted within the component library
+    //inherit dbcontext for entity framework
+    //requires System.Data.Entity
+   internal class ChinookContext:DbContext
+    {
+        //Pass the connection string name to the 
+        //DbContext using the :base()
+        public ChinookContext(): base("chinookDB")
+        {
+
+        }
+
+        //setup Dbset properties
+        public DbSet<Artist>Artists { get; set; }
+        public DbSet<Album>Albums { get; set; }
+        public DbSet<Track>Tracks { get; set; }
+        public DbSet<MediaType>MediaTypes { get; set; }
+    }
+}
